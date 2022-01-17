2ef714851743ff99b752cfcb04e27b195081eab0
LibXZR <i@xzr.moe>
2022-01-17 14:26:00 +0800

    Show Kaleidoscope main version in firmware version
    Change-Id: If57fcbabbc8cfadcabed9700fb21717d27eae9b9
    Signed-off-by: LibXZR <i@xzr.moe>

---
+++ src/com/android/settings/deviceinfo/firmwareversion/KscopeVersionPreferenceController.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2022 Project Kaleidoscope
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.android.settings.deviceinfo.firmwareversion;
+
+import android.content.Context;
+import android.os.SystemProperties;
+
+import androidx.preference.Preference;
+
+import com.android.settings.core.BasePreferenceController;
+
+public class KscopeVersionPreferenceController extends BasePreferenceController {
+
+    private String mKscopeVersion;
+
+    private String getKscopeMainVersion(){
+        String[] fullVersion = SystemProperties.get("ro.kscope.build.version").split("-");
+        return fullVersion.length > 1 ? fullVersion[1] : null;
+    }
+
+    public KscopeVersionPreferenceController(Context context, String key) {
+        super(context, key);
+        mKscopeVersion = getKscopeMainVersion();
+    }
+
+    @Override
+    public int getAvailabilityStatus() {
+        return mKscopeVersion != null
+                ? AVAILABLE : CONDITIONALLY_UNAVAILABLE;
+    }
+
+    @Override
+    public CharSequence getSummary() {
+        return mKscopeVersion;
+    }
+}
